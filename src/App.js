1cb3c70f8521428a18334a6c7bfce7ac6681cb39
collinskoech11 <collinskoechck34@gmail.com>
2020-07-17 16:00:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="demo-big-content">
+      <Layout>
+          <Header title="Title" scroll>
+              <Navigation>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+              </Navigation>
+          </Header>
+          <Drawer title="Title">
+              <Navigation>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+                  <a href="/">Link</a>
+              </Navigation>
+          </Drawer>
+          <Content>
+              <div className="page-content" />
+          </Content>
+      </Layout>
+  </div>
     );
   }
 }
